4b34c7198b639b026e04f0cebe6c36c83e1dca62
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2024-03-02 03:55:52 -0500

    initial version of t.py - it has bug

---
+++ t.py
@@ -0,0 +1,9 @@
+def get_travel_time_ms(distance, speed):
+    return distance / speed # this is the bug: output in secs instead of millisecs
+
+if __name__ == "__main__":
+    distance = 100 # m
+    speed = 1 # m/s
+    print("travel distance:", distance)
+    print("travel speed:", speed)
+    print("travel time:", get_travel_time_ms(distance, speed), " ms")
